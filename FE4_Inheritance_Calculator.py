86027fc85469293002a37366788bc0a8cfe59515
LukeSev <lloughner00@gmail.com>
2023-11-27 22:16:37 -0800

    Cleaned up main application

--- FE4_Inheritance_Calculator.py
+++ FE4_Inheritance_Calculator.py
@@ -7,15 +7,5 @@ def main():
     window = FE4_Calc()
     sys.exit(app.exec())
 
-def test():
-    main_parent = Stats("Sigurd", "Parent", 40, 20, 5, 20, 20, 10, 20, 10)
-    main_lvl = 30
-    second_parent = Stats("Deirdre", "Parent", 15, 5, 30, 15, 15, 5, 5, 30)
-    second_lvl = 30
-
-    child = calc_start_stats(main_parent, 1, second_parent, 0, "Seliph", "Sigurd")
-
-    child.print_stats()
-
 if __name__ == "__main__":
     main()
